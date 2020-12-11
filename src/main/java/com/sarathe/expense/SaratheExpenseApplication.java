191229290997506ff4146df22e97bc32dba7d199
siddharth2223437 <siddharthsarathe.nmims@gmail.com>
2020-12-10 18:47:46 -0600

    setting up project

---
+++ src/main/java/com/sarathe/expense/SaratheExpenseApplication.java
@@ -0,0 +1,13 @@
+package com.sarathe.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaratheExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaratheExpenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarathe/expense/SaratheExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarathe.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaratheExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
